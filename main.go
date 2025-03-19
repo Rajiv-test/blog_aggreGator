0cc307633719fb06905ec84363c6391fb3c99e2e
Rajiv-test <tathireddyrajivreddy@gmail.com>
2025-03-19 18:41:12 +0530

    added database

--- main.go
+++ main.go
@@ -1,24 +1,55 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/Rajiv-test/blog_aggreGator/internal/config"
+	"github.com/Rajiv-test/blog_aggreGator/internal/database"
+	_ "github.com/lib/pq"
 )
+
+type state struct {
+	db  *database.Queries
+	cfg *config.Config
+}
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Printf("Read config: %+v\n", cfg)
-
-	err = cfg.SetUser("lane")
-
-	cfg, err = config.Read()
+	fmt.Println(cfg.DBURL)
+	db, err := sql.Open("postgres", cfg.DBURL)
 	if err != nil {
-		log.Fatalf("error reading config: %v", err)
+		log.Fatalf("error connecting to db: %v", err)
 	}
-	fmt.Printf("Read config again: %+v\n", cfg)
+	defer db.Close()
+	dbQueries := database.New(db)
+
+	programState := &state{
+		db:  dbQueries,
+		cfg: &cfg,
+	}
+
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: cli <command> [args...]")
+		return
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
